57ed0af85252d05c419559815253bea1cf0b96a3
John Cooper <john@choffee.co.uk>
2013-12-18 19:46:50 +0000

    Inital spark core http server

---
+++ test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+        "github.com/hybridgroup/gobot"
+        "github.com/hybridgroup/gobot-spark"
+        "github.com/hybridgroup/gobot-gpio"
+//        "time"
+        "fmt"
+        "net/http"
+)
+
+// Used for to pass a chanel to the handler
+
+func handler(controlChan chan int, w http.ResponseWriter, r *http.Request) {
+    controlChan <- 1
+    fmt.Fprintf(w, "<html><head><body><form name=SetLights, action=\"/lights\", method=\"post\"><input type=\"radio\" name=\"light\">Red</input><input type=\"submit\" name=\"Set\"></form></body></html>")
+}
+
+
+func main() {
+
+        var controlChan = make(chan int)
+
+        spark := new(gobotSpark.SparkAdaptor)
+        spark.Name = "spark"
+        spark.Params = make(map[string]interface{})
+        spark.Params["device_id"] = ""
+        spark.Params["access_token"] = ""
+
+        led := gobotGPIO.NewLed(spark)
+        led.Name = "led"
+        led.Pin = "D7"
+
+
+        work := func() {
+            //gobot.Every("1s", func() {
+            //            led.Toggle()
+            //})
+            for {
+                _ = <- controlChan
+                led.Toggle()
+            }
+
+        }
+
+        robot := gobot.Robot{
+                Connections: []interface{}{ spark }, 
+                Devices:     []interface{}{ led },
+                Work:        work,
+        }
+
+        go robot.Start()
+        //for {
+            //control <- 1
+            //time.Sleep(1000 * time.Millisecond)
+        //}
+
+
+        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+            handler(controlChan, w, r)
+        })
+
+        http.ListenAndServe(":8080", nil)
+
+}
