2ef6b9760c1b4cae0359e43d8568ba3d2e448786
John Cooper <john@choffee.co.uk>
2013-12-19 08:26:01 +0000

    gofmt the code

--- test.go
+++ test.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+    "github.com/hybridgroup/gobot"
+    "github.com/hybridgroup/gobot-gpio"
+    "github.com/hybridgroup/gobot-spark"
     "github.com/jimlawless/cfg"
-        "github.com/hybridgroup/gobot"
-        "github.com/hybridgroup/gobot-spark"
-        "github.com/hybridgroup/gobot-gpio"
-//        "time"
-        "fmt"
-        "net/http"
-        "log"
+    //        "time"
+    "fmt"
+    "log"
+    "net/http"
 )
 
 // Used for to pass a chanel to the handler
@@ -17,7 +17,6 @@ func handler(controlChan chan int, w http.ResponseWriter, r *http.Request) {
     controlChan <- 1
     fmt.Fprintf(w, "<html><head><body><form name=SetLights, action=\"/lights\", method=\"post\"><input type=\"radio\" name=\"light\">Red</input><input type=\"submit\" name=\"Set\"></form></body></html>")
 }
-
 
 func main() {
 
@@ -29,47 +28,45 @@ func main() {
     }
     fmt.Printf("%v\n", config)
 
-        var controlChan = make(chan int)
+    var controlChan = make(chan int)
 
-        spark := new(gobotSpark.SparkAdaptor)
-        spark.Name = "spark"
-        spark.Params = make(map[string]interface{})
-        spark.Params["device_id"] = config["device_id"]
-        spark.Params["access_token"] = config["access_token"]
+    spark := new(gobotSpark.SparkAdaptor)
+    spark.Name = "spark"
+    spark.Params = make(map[string]interface{})
+    spark.Params["device_id"] = config["device_id"]
+    spark.Params["access_token"] = config["access_token"]
 
-        led := gobotGPIO.NewLed(spark)
-        led.Name = "led"
-        led.Pin = "D7"
+    led := gobotGPIO.NewLed(spark)
+    led.Name = "led"
+    led.Pin = "D7"
 
-
-        work := func() {
-            //gobot.Every("1s", func() {
-            //            led.Toggle()
-            //})
-            for {
-                _ = <- controlChan
-                led.Toggle()
-            }
-
+    work := func() {
+        //gobot.Every("1s", func() {
+        //            led.Toggle()
+        //})
+        for {
+            _ = <-controlChan
+            led.Toggle()
         }
 
-        robot := gobot.Robot{
-                Connections: []interface{}{ spark }, 
-                Devices:     []interface{}{ led },
-                Work:        work,
-        }
+    }
 
-        go robot.Start()
-        //for {
-            //control <- 1
-            //time.Sleep(1000 * time.Millisecond)
-        //}
+    robot := gobot.Robot{
+        Connections: []interface{}{spark},
+        Devices:     []interface{}{led},
+        Work:        work,
+    }
 
+    go robot.Start()
+    //for {
+    //control <- 1
+    //time.Sleep(1000 * time.Millisecond)
+    //}
 
-        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-            handler(controlChan, w, r)
-        })
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        handler(controlChan, w, r)
+    })
 
-        http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":8080", nil)
 
 }
